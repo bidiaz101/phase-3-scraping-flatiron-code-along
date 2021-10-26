ff87c64915d4e4ce05062ff3cdc973aa1809eb2a
Brian Diaz <bidiaz101@gmail.com>
2021-10-26 00:03:04 -0500

    Completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,8 +15,23 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    Nokogiri::HTML(URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |course|
+      new_course = Course.new
+      new_course.title = course.css("h2").text
+      new_course.schedule = course.css(".date").text
+      new_course.description = course.css("p").text
+    end
+  end
 end
 
-
-
+Scraper.new.print_courses
